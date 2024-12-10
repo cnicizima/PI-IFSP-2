fe19141e9e26c9c8277e7cad211ff5033d6d4e22
Ciro Nicizima <cironicizima@gmail.com>
2024-12-09 23:05:59 -0300

    ultimos ajustes - botao pagamento "funcionando"

--- public/js/contato.js
+++ public/js/contato.js
@@ -10,3 +10,9 @@ document.querySelectorAll('.faq-item').forEach(item => {
         item.classList.toggle('active');
     });
 });
+
+document.querySelector('.btn_entre_em_contato').addEventListener('click', (event) => {
+    event.preventDefault(); // Evita o comportamento padrão do botão dentro de formulários
+    alert("Mensagem enviada com sucesso!");
+    location.reload();
+});
